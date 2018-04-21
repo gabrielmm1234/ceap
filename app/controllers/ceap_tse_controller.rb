25220010d41ffa65e8cc589044e6aa98284a7353
Gabriel Mesquita de Araujo <gabrieljustware@gmail.com>
2018-04-21 17:12:19 -0300

    Add httpparty gem and tested http call to orientdb server

---
+++ app/controllers/ceap_tse_controller.rb
@@ -0,0 +1,9 @@
+class CeapTseController < ApplicationController
+  def index
+    auth = {username: "admin", password: "admin"}
+    response = HTTParty.get('http://localhost:2481/class/CotasParlamentares/Parlamentar', basic_auth: auth)
+    @parlamentar_count = JSON.parse(response.body)["records"]
+    response = HTTParty.get('http://localhost:2481/class/CotasParlamentares/EmpresaFornecedora', basic_auth: auth)
+    @empresa_fornecedora_count = JSON.parse(response.body)["records"]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'home/index'
+  resources :ceap_tse, only: [:index]
 end
