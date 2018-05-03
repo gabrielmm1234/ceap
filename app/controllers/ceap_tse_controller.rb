5a6e2260e096ffad347780103dcb295ce8b9d5c4
Gabriel Mesquita de Araujo <gabrieljustware@gmail.com>
2018-05-03 09:00:06 -0300

    Add style to ceap_tse screen

--- app/controllers/ceap_tse_controller.rb
+++ app/controllers/ceap_tse_controller.rb
@@ -5,5 +5,7 @@ class CeapTseController < ApplicationController
     @parlamentar_count = JSON.parse(response.body)["records"]
     response = HTTParty.get('http://localhost:2481/class/CotasParlamentares/EmpresaFornecedora', basic_auth: auth)
     @empresa_fornecedora_count = JSON.parse(response.body)["records"]
+    response = HTTParty.get('http://localhost:2481/class/CotasParlamentares/Transacao', basic_auth: auth)
+    @transacoes_count = JSON.parse(response.body)["records"]
   end
 end
