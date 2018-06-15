c433fea500130690fa74e53cc6a8429901fa6d5a
Gabriel Mesquita de Araujo <gabrieljustware@gmail.com>
2018-06-14 23:40:38 -0300

    Add graph of CEAP/TSE

--- app/assets/javascripts/ceap_tse.js
+++ app/assets/javascripts/ceap_tse.js
@@ -4,41 +4,94 @@ $( document ).ready(function() {
     type: "GET",
     url: "/donations_ceap_tse",
     success: function(data){
-      console.log(data)
+      var s = new sigma('container-graph');
+      var label = undefined
+      var color = undefined
+
+      data["result"].forEach(function(item) {
+        if (item["@class"] == "Transacao") {
+          color = '#f00'
+          label = item['TxtDescricao'] + " - R$ " + item["VlrReceita"]
+        } else if(item["@class"] == "Parlamentar") {
+          color = '#00f'
+          label = item['TxNomeParlamentar']
+        } else {
+          color = '#ec5148'
+          label = item['TxtFornecedor']
+        }
+
+        s.graph.addNode({
+          id: item["@rid"],
+          label: label,
+          x: Math.random(),
+          y: Math.random(),
+          size: 1,
+          color: color
+        })
+      })
+
+      var transactions = data["result"].filter(function(item) { return item["@class"] == "Transacao" })
+      var deputies = data["result"].filter(function(item) { return item["@class"] == "Parlamentar" })
+      var companies = data["result"].filter(function(item) { return item["@class"] == "EmpresaFornecedora" })
+
+      transactions.forEach(function(item) {
+        var supplier = undefined
+        var deputy = undefined
+        var company = undefined
+
+        deputy = deputies.filter(function(deputy_item) {
+          return deputy_item["out_RealizaTransacao"].includes(item["in_RealizaTransacao"][0])
+        })
+
+        company = companies.filter(function(company_item) {
+          return company_item["out_RealizaTransacao"].includes(item["in_RealizaTransacao"][0])
+        })
+
+        if (deputy.length != 0) {
+          s.graph.addEdge({
+            id: deputy[0]["@rid"] + item["@rid"],
+            source: deputy[0]["@rid"],
+            target: item["@rid"]
+          });
+        } else {
+          s.graph.addEdge({
+            id: company[0]["@rid"] + item["@rid"],
+            source: company[0]["@rid"],
+            target: item["@rid"]
+          });
+        }
+
+        if (item["out_FornecidaPor"] != undefined) {
+          company = companies.filter(function(company_item) {
+            return company_item["in_FornecidaPor"].includes(item["out_FornecidaPor"][0])
+          })
+
+          if (company.length != 0) {
+            s.graph.addEdge({
+              id: company[0]["@rid"] + item["@rid"],
+              source: item["@rid"],
+              target: company[0]["@rid"]
+            });
+          }
+        } else {
+          deputy = deputies.filter(function(deputy_item) {
+            return deputy_item["in_FornecidaPara"].includes(item["out_FornecidaPara"][0])
+          })
+
+          if (deputy.length != 0) {
+            s.graph.addEdge({
+              id: deputy[0]["@rid"] + item["@rid"],
+              source: item["@rid"],
+              target: deputy[0]["@rid"]
+            });
+          }
+        }
+      })
+      s.refresh();
+      s.startForceAtlas2({worker: true,adjustSizes:true,scalingRatio: 1,outboundAttractionDistribution:true});
     }
   });
 
-    // Let's first initialize sigma:
-    var s = new sigma('container-graph');
-
-    // Then, let's add some data to display:
-    s.graph.addNode({
-      // Main attributes:
-      id: 'n0',
-      label: 'Hello',
-      // Display attributes:
-      x: 0,
-      y: 0,
-      size: 1,
-      color: '#f00'
-    }).addNode({
-      // Main attributes:
-      id: 'n1',
-      label: 'World !',
-      // Display attributes:
-      x: 0,
-      y: 1,
-      size: 1,
-      color: '#00f'
-    }).addEdge({
-      id: 'e0',
-      // Reference extremities:
-      source: 'n0',
-      target: 'n1'
-    });
-
-    // Finally, let's ask our sigma instance to refresh:
-    s.refresh();
 
   $.ajax({
     type: "GET",
